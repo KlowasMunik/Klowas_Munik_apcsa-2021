3be716627baf15d0f06491eab15e57770c1bbbba
KlowasMunik <78110073+KlowasMunik@users.noreply.github.com>
2021-02-03 21:51:50 -0800

    all

---
+++ Unit00/src/Variables.java
@@ -0,0 +1,54 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Variables
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte		short		int 		long
+		//float		double
+		//char      boolean		String
+
+		//integer variables
+		byte byteOne = 127;
+		short shortOne = 1;
+
+		//decimal variables
+		int intOne = 121;
+
+		//other integer types
+		char charOne = 727;
+		boolean boolOne = true;
+		String lolOne = ("hey");
+		
+
+		//other types
+
+
+		//output your information here
+		System.out.println("/////////////////////////////////");
+		System.out.println("*                               *");
+		System.out.println("*        integer types          *");
+		System.out.println("*                               *");
+		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
