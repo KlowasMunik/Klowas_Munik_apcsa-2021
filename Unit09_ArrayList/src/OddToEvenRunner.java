9dfef44841bd0542322f8811aeb7e4566edfc776
KlowasMunik <78110073+KlowasMunik@users.noreply.github.com>
2021-03-10 23:18:36 -0800

    zxc

---
+++ Unit09_ArrayList/src/OddToEvenRunner.java
@@ -0,0 +1,22 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+
+public class OddToEvenRunner
+{
+	public static int go( List<Integer> ray )
+	{
+		for ( int start = 0; start < ray.size(); start ++ )
+		{
+			if ( ray.get(start)%2 > 0 )
+			{	for ( int even = ray.size()-1; even > ray.indexOf(start); even -- )
+					if ( ray.get(even)%2 == 0 )
+				{
+					return (ray.lastIndexOf(ray.get(even)) - ray.indexOf(ray.get(start)));
+				}
+			}		
+		}
+		return -1;
+	}
+}
